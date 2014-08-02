afaef6523dd8f04500b23f5a0df21f546ee9af6e
Josh Newlan <joshuanewlan@gmail.com>
2014-08-02 22:46:06 +0800

    Update reddit-is.py

--- reddit-is.py
+++ reddit-is.py
@@ -1,28 +1,29 @@
 import praw
 import re
 
-r = praw.Reddit('Reddit is /u/user')
-r.login('user', 'pwd')
-textfile = "/Users/user/reddit-is.txt"
+r = praw.Reddit('Reddit is /u/USER')
+r.login('USERNAME', 'PASSWORD')
 
 comments = praw.helpers.comment_stream(r, 'all', limit=None)
 
 for comment in comments:
-    p = re.compile(ur'(reddit\s+(is|isnt|isn\'t|was|wasnt|wasn\'t|does|didnt|didn\'t|says|said|has|hasnt|hasn\'t|can|cannot|cant|can\'t|will|wont|won\'t|likes|dislikes|hates|loves|thinks|hopes|acts|wishes|gets|wants|needs|fears|worships)\b)', re.IGNORECASE)
+    p = re.compile(ur'(reddit\s+(is|isnt|isn\'t|was|wasnt|wasn\'t|does|didnt|didn\'t|says|said|has|hasnt|hasn\'t|can|cannot|cant|can\'t|will|wont|won\'t|likes|dislikes|hates|loves|thinks|hopes|acts|wishes|gets|wants|needs|fears|worships|finds|pretends)\b)', re.IGNORECASE)
+    #tis = re.findall(p, comment.body)
     tis = re.match(p, comment.body)
     if tis is not None:
         body = comment.body.rstrip('\n')
-        with open(textfile,'a') as f:
-            f.write(comment.id + ",")
-            f.write(comment.link_id + ",")
+        with open('/Users/user/test.txt','a') as f:
+            comment_id = str(comment.id)
             subreddit = str(comment.subreddit)
             author = str(comment.author)
+            link = str(comment.permalink)
+            score = str(comment.score)
+            created_utc = str(comment.created_utc)
+            controversiality = str(comment.controversiality)
+            f.write(comment_id + ",")
             f.write(subreddit + ",")
             f.write(author + ",")
-            score = str(comment.score)
-            controversiality = str(comment.controversiality)
-            created_utc = str(comment.created_utc)
-            f.write(score+ "," +controversiality+ "," +created_utc+ "," +body)
+            f.write(score+ "," +controversiality+ "," +created_utc+ "," +link+ ",reddit-is-comment:" +body)
             f.write('\n')
         f.closed
         print comment.body
