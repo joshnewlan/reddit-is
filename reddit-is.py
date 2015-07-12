fc6277c730f15862461361fdfbc51bead865d71c
Josh Newlan <joshuanewlan@gmail.com>
2015-07-12 14:52:21 +0800

    Added some error handling and oauth

--- reddit-is.py
+++ reddit-is.py
@@ -1,31 +1,67 @@
 import praw
 import re
+import time
 
-r = praw.Reddit('Reddit is /u/USER')
-r.login('USERNAME', 'PASSWORD')
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REDIRECT_URI = ""
+REFRESH_TOKEN = ""
+USERNAME = ""
 
-comments = praw.helpers.comment_stream(r, 'all', limit=None)
+r = praw.Reddit('Reddit is /u/{}'.format(USERNAME))
+r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-for comment in comments:
-    p = re.compile(ur'(reddit\s+(is|isnt|isn\'t|was|wasnt|wasn\'t|does|didnt|didn\'t|says|said|has|hasnt|hasn\'t|can|cannot|cant|can\'t|will|wont|won\'t|likes|dislikes|hates|loves|thinks|hopes|acts|wishes|gets|wants|needs|fears|worships|finds|pretends|compares)\b)', re.IGNORECASE)
-    #tis = re.findall(p, comment.body)
-    tis = re.match(p, comment.body)
-    if tis is not None:
-        body = comment.body.rstrip('\n')
-        with open('test.txt','a') as f:
-            comment_id = str(comment.id)
-            subreddit = str(comment.subreddit)
-            author = str(comment.author)
-            link = str(comment.permalink)
-            score = str(comment.score)
-            created_utc = str(comment.created_utc)
-            controversiality = str(comment.controversiality)
-            f.write(comment_id + ",")
-            f.write(subreddit + ",")
-            f.write(author + ",")
-            f.write(score+ "," +controversiality+ "," +created_utc+ "," +link+ ",reddit-is-comment:" +body)
-            f.write('\n')
-        f.closed
-        print comment.body
-    else:
-        continue
+print "Authenticating"
+access_information = r.refresh_access_information(REFRESH_TOKEN)
+r.set_access_credentials(**access_information)
+authenticated_user = r.get_me()
+print "Authenticated as {}".format(authenticated_user.name)
+    
+# TODO: Check authenticated_user.link_karma > 1
+
+try:
+    while True:
+        try:
+            comments = praw.helpers.comment_stream(r, 'all', limit=None,verbosity=3)
+        except ConnectionError:
+            print "Connection Error, waiting..."
+            time.wait(10)
+            print "Retrying..."
+            pass
+
+        for comment in comments:
+            p = re.compile(ur"""(reddit\s+(is|isnt|isn\'t|
+                                    was|wasnt|wasn\'t|werent|weren\'t|
+                                    does|didnt|didn\'t|goes|makes|gets|
+                                    will|wont|won\'t|
+                                    says|said|pontificates|
+                                    has|hasnt|hasn\'t|
+                                    can|cannot|cant|can\'t|
+                                    likes|dislikes|hates|loves|
+                                    thinks|hopes|acts|wishes|wants|needs|
+                                    fears|freaks|worships|finds|pretends|compares|
+                                    spazzes|denies|sings)\b)""", re.IGNORECASE)
+            tis = re.match(p, comment.body)
+            if tis is not None:
+                body = comment.body.rstrip('\n')
+                with open('reddit-is.csv','a') as f:
+                    comment_id = str(comment.id)
+                    subreddit = str(comment.subreddit)
+                    author = str(comment.author)
+                    link = str(comment.permalink)
+                    score = str(comment.score)
+                    created_utc = str(comment.created_utc)
+                    controversiality = str(comment.controversiality)
+
+                    f.write(comment_id + ",")
+                    f.write(subreddit + ",")
+                    f.write(author + ",")
+                    f.write(score + "," + controversiality + "," + created_utc + "," 
+                        +link+ ",reddit-is-comment:" +body)
+                    f.write('\n')
+                f.closed
+                print comment.body
+            else:
+                continue
+except KeyboardInterrupt:
+    pass
